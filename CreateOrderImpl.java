079849fdeac03271616fc96818b08e501ef5af31
sgtpanic <30394101+sgtpanic@users.noreply.github.com>
2018-02-06 18:03:43 -0500

    add synchronous code

--- CreateOrderImpl.java
+++ CreateOrderImpl.java
@@ -1,9 +1,12 @@
 package com.influentialcode;
 
 import com.google.inject.Inject;
+import com.influentialcode.exceptions.QuantityUnavailableException;
 import com.influentialcode.persistence.CatalogDao;
 import com.influentialcode.persistence.PurchasesDao;
 import com.influentialcode.persistence.WarehouseDao;
+
+import java.time.Instant;
 
 public class CreateOrderImpl extends CreateOrder {
 
@@ -16,5 +19,28 @@ public class CreateOrderImpl extends CreateOrder {
     public void createOrder() {
         LOGGER.info("Beginning to create order synchronously");
 
+        long startTime = Instant.now().getEpochSecond();
+
+        LOGGER.info("Beginning to query warehouse");
+        int quantityAvailable = warehouseDao.getQuantity(PRODUCT_ID);
+        LOGGER.info("Finished querying warehouse.");
+
+        if (quantityAvailable < QUANTITY_DESIRED) {
+            LOGGER.error("There isn't enough quantity available");
+            throw new QuantityUnavailableException();
+        }
+
+        LOGGER.info("Beginning to query catalog for price.");
+        int itemPrice = catalogDao.getPrice(PRODUCT_ID);
+        LOGGER.info("Finished querying catalog.");
+
+        int totalPrice = itemPrice * quantityAvailable;
+
+        LOGGER.info("Adding purchase order.");
+        purchaseDao.addPurchase(CUSTOMER_ID, totalPrice);
+        LOGGER.info("Purchase order created.");
+
+        long endTime = Instant.now().getEpochSecond();
+        LOGGER.info("Total processing time: {}", endTime - startTime);
     }
 }
