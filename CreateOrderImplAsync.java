e5caf5f1ee69a18071652e29e45facaf0982883d
sgtpanic <30394101+sgtpanic@users.noreply.github.com>
2018-02-12 08:27:24 -0500

    Use skeleton for async portion

--- CreateOrderImplAsync.java
+++ CreateOrderImplAsync.java
@@ -1,15 +1,15 @@
 package com.influentialcode;
 
 import com.google.inject.Inject;
+import com.influentialcode.exceptions.QuantityUnavailableException;
 import com.influentialcode.persistence.CatalogDao;
 import com.influentialcode.persistence.PurchasesDao;
 import com.influentialcode.persistence.WarehouseDao;
 
-public class CreateOrderImplAsync extends CreateOrder {
-    private CatalogDao catalogDao;
-    private PurchasesDao purchaseDao;
-    private WarehouseDao warehouseDao;
+import java.time.Instant;
+import java.util.concurrent.CompletableFuture;
 
+public class CreateOrderImplAsync extends CreateOrder {
     @Inject
     public CreateOrderImplAsync(CatalogDao catalogDao, PurchasesDao purchaseDao, WarehouseDao warehouseDao) {
         super(catalogDao, purchaseDao, warehouseDao);
@@ -17,6 +17,30 @@ public class CreateOrderImplAsync extends CreateOrder {
 
     @Override
     public void createOrder() {
+        LOGGER.info("Beginning to create order synchronously");
+        long startTime = Instant.now().getEpochSecond();
 
+        LOGGER.info("Beginning to query warehouse");
+
+        LOGGER.info("Finished querying warehouse.");
+
+        /*
+        if (quantityAvailable < QUANTITY_DESIRED) {
+            LOGGER.error("There isn't enough quantity available");
+            throw new QuantityUnavailableException();
+        }
+        */
+
+        LOGGER.info("Beginning to query catalog for price.");
+
+        LOGGER.info("Finished querying catalog.");
+
+
+        LOGGER.info("Adding purchase order.");
+
+        LOGGER.info("Purchase order created.");
+
+        long endTime = Instant.now().getEpochSecond();
+        LOGGER.info("Total processing time: {}", endTime - startTime);
     }
 }
--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ import com.influentialcode.guice.StoreModule;
 public class Main {
 
     public static void main(String[] args) {
-        CreateOrder createOrder = Guice.createInjector(new StoreModule()).getInstance(CreateOrder.class);
+        CreateOrder createOrder = Guice.createInjector(new StoreModule()).getInstance(CreateOrderImpl.class);
         createOrder.createOrder();
     }
 
